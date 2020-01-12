b454316c743e3a846a0b780fe57d6c7d8452c408
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2020-01-12 19:24:11 +0000

    Commit

--- app/src/main/java/com/moondevs/moon/home_screens/shops_screens/ShopItem.kt
+++ app/src/main/java/com/moondevs/moon/home_screens/shops_screens/ShopItem.kt
@@ -2,21 +2,21 @@ package com.moondevs.moon.home_screens.shops_screens
 
 class ShopItem {
 
-    var shopItemId : String = ""
-    var shopItemImage : String = ""
-    var shopItemName: String = ""
-    var shopItemPrice: String = ""
-    var shopItemCount : Int = 0
+     var shopItemId : String = ""
+     var shopItemImage : String = ""
+     var shopItemName: String = ""
+     var shopItemPrice: String = ""
+     var shopItemCount : Int = 0
 
 
-    constructor() {}
+     constructor() {}
 
-    constructor(shopItemId: String, shopItemImage: String, shopItemName: String,
-                shopItemPrice: String, shopItemCount: Int) : this() {
-        this.shopItemId = shopItemId
-        this.shopItemImage = shopItemImage
-        this.shopItemName = shopItemName
-        this.shopItemPrice = shopItemPrice
-        this.shopItemCount = shopItemCount
-    }
+     constructor(shopItemId: String, shopItemImage: String, shopItemName: String,
+                 shopItemPrice: String, shopItemCount: Int) : this() {
+          this.shopItemId = shopItemId
+          this.shopItemImage = shopItemImage
+          this.shopItemName = shopItemName
+          this.shopItemPrice = shopItemPrice
+          this.shopItemCount = shopItemCount
+     }
 }
