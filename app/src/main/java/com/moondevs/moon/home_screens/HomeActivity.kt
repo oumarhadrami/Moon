d2d768481948b7815fefee53b66a294eba3c0725
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2020-01-16 17:07:00 +0000

    Add Badge to cart showing number of items in cart

--- app/src/main/java/com/moondevs/moon/home_screens/HomeActivity.kt
+++ app/src/main/java/com/moondevs/moon/home_screens/HomeActivity.kt
@@ -1,22 +1,39 @@
 package com.moondevs.moon.home_screens
 
+import android.app.Application
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.badge.BadgeDrawable
 import com.moondevs.moon.R
+import com.moondevs.moon.home_screens.shops_screens.ShoppingCartViewModel
+import com.moondevs.moon.home_screens.shops_screens.ShoppingCartViewModelFactory
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var navView: BottomNavigationView
+    private lateinit var shoppingCartViewModel: ShoppingCartViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        /**initialize ShoppingCartViewModel*/
+        val application : Application = requireNotNull(this).application
+        val viewModelFactory = ShoppingCartViewModelFactory(application)
+        shoppingCartViewModel = ViewModelProviders.of(this, viewModelFactory).get(ShoppingCartViewModel::class.java)
+
+
+        navView = findViewById(R.id.nav_view)
 
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -32,6 +49,20 @@ class HomeActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        addBadgeToCart()
+    }
+
+    private fun addBadgeToCart() {
+        val badge : BadgeDrawable  = navView.getOrCreateBadge(R.id.navigation_cart)
+        shoppingCartViewModel.allItemsCount.observe(this, Observer {allItemsCount ->
+            badge.number = allItemsCount
+            badge.isVisible = allItemsCount != 0
+        })
+
+        badge.backgroundColor = Color.parseColor("#f8ab2e")
+        badge.badgeTextColor = Color.parseColor("#ffffff")
     }
 
 
