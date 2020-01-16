38580b3e01185cbfb53ef56fe6569ec977ce0588
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2020-01-16 01:00:35 +0000

    Getting Account Fragment ready

--- app/src/main/java/com/moondevs/moon/home_screens/account/AccountViewModel.kt
+++ app/src/main/java/com/moondevs/moon/home_screens/account/AccountViewModel.kt
@@ -1,7 +1,5 @@
 package com.moondevs.moon.home_screens.account
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AccountViewModel : ViewModel() {
