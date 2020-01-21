b7382f9dba915ee23ab7dc4cade72fc55f6feaef
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2020-01-21 16:43:27 +0000

    Add search UI to SearchFragment

--- app/src/main/java/com/moondevs/moon/home_screens/search/SearchFragment.kt
+++ app/src/main/java/com/moondevs/moon/home_screens/search/SearchFragment.kt
@@ -1,13 +1,16 @@
 package com.moondevs.moon.home_screens.search
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.textfield.TextInputEditText
 import com.moondevs.moon.R
 import com.moondevs.moon.databinding.FragmentSearchBinding
 
@@ -28,6 +31,30 @@ class SearchFragment : Fragment() {
         super.onStart()
         //add content for toolbar
         val frameLayout = activity!!.findViewById<View>(R.id.toolbar_framelayout)
+        val searchLayout = frameLayout.findViewById<View>(R.id.search_page_layout)
+        val searchEditText = searchLayout.findViewById<TextInputEditText>(R.id.search_shops_textfield)
+        frameLayout.visibility = View.VISIBLE
+        searchLayout.visibility = View.VISIBLE
+
+        /**make keyboard appear*/
+        searchEditText.requestFocus()
+        val imm = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm!!.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val frameLayout = activity!!.findViewById<View>(R.id.toolbar_framelayout)
+        val searchLayout = frameLayout.findViewById<View>(R.id.search_page_layout)
+        val searchEditText = searchLayout.findViewById<TextInputEditText>(R.id.search_shops_textfield)
         frameLayout.visibility = View.GONE
+        searchLayout.visibility = View.GONE
+
+        /**make keyboard disappear */
+        searchEditText.requestFocus()
+        val imm = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm!!.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+
     }
 }
